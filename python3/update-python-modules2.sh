1a3915d5b3348d136db631d59a5067dd2a7c17c8
Chris1320 <chris1320is@protonmail.com>
2022-04-02 18:31:06 +0800

    Deleted old Python package update script
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- python3/update-python-modules2.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-
-echo "Installing essentials..."
-pip install --upgrade pip wheel build setuptools twine
-echo "Installing frequently used..."
-pip install --upgrade flake8 requests tqdm youtube-dl pytest
-echo "Done."
-exit 0
