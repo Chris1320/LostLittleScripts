16b1ac20b4d2495e16985a88078bfd10d9e41bdb
Chris1320 <chris1320is@protonmail.com>
2022-04-03 23:44:09 +0800

    Added popsicles coding problem
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

---
+++ playground/csharp/popsicles.cs
@@ -0,0 +1,51 @@
+/*
+ * Popsicles
+ *
+ * You have a box of popsicles and you want to give them all away to a group of
+ * brothers and sisters. If you have enough left in the box to give them each an
+ * even amount you should go for it! If not, they will fight over them, and you
+ * should eat them yourself later.
+ *
+ * Task
+ * Given the number of siblings that you are giving popsicles to, determine if
+ * you can give them each an even amount or if you shouldn't mention the
+ * popsicles at all.
+ *
+ * Input Format
+ * Two integer values, the first one represents the number of siblings, and the
+ * second one represents the number of popsicles that you have left in the box.
+ *
+ * Output Format
+ * A string that says 'give away' if you are giving them away, or 'eat them
+ * yourself' if you will be eating them yourself.
+ *
+ * Sample Input
+ * 3 9
+ * 
+ * Sample Output
+ * give away
+ */
+
+using System;
+
+namespace Popsicles
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            int siblings, popsicles;  // Declare variables
+            
+            // Read user input
+            siblings = Convert.ToInt32(Console.ReadLine());
+            popsicles = Convert.ToInt32(Console.ReadLine());
+            
+            // Validate
+            if (popsicles % siblings == 0) Console.WriteLine("give away");
+            else Console.WriteLine("eat them yourself");
+            
+            return 0;
+        }
+    }
+}
+
