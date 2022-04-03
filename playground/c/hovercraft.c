4ae031cb4d55962f2edea0568dc4bcb2556b080e
Chris1320 <chris1320is@protonmail.com>
2022-04-03 08:53:34 +0800

    Added more C and Python code
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

---
+++ playground/c/hovercraft.c
@@ -0,0 +1,47 @@
+/*
+ * Hovercraft
+ *
+ * You run a hovercraft factory. Your factory makes ten hovercrafts in a month. Given
+ * the number of customers you got that month, did you make a profit? It costs you
+ * 2,000,000 to build a hovercraft, and you are selling them for 3,000,000. You
+ * also pay 1,000,000 each month for insurance.
+ *
+ * Task:
+ * Determine whether or not you made a profit based on how many of the ten
+ * hovercrafts you were able to sell that month.
+ *
+ * Input Format:
+ * An integer that represents the sales that you made that month.
+ *
+ * Output Format:
+ * A string that says 'Profit', 'Loss', or 'Broke Even'.
+ *
+ * Sample Input:
+ * 5
+ *
+ * Sample Output:
+ * Loss
+ */
+
+#include <stdio.h>
+
+int main() {
+    int sales, _;
+    float profit, monthly_cost = 0, monthly_sale = 0, cost = 2000000, sale = 3000000, expense = 1000000;
+    monthly_cost = cost * 10;
+    
+    printf("How many sales did you make this month? > ");
+    scanf("%i", &sales);
+
+    for (_ = 0; _ < sales; _++)
+    {
+        monthly_sale += sale;
+    }
+    profit = (monthly_sale - monthly_cost) - expense;
+    printf("%f\n", profit);
+    if (profit > 0) printf("Profit\n");
+    else if (profit < 0) printf("Loss\n");
+    else printf("Broke Even\n");
+    return 0;
+}
+
