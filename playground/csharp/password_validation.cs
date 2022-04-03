6bbe7b3976d16fff58e91a206f488f059892103b
Chris1320 <chris1320is@protonmail.com>
2022-04-03 11:53:10 +0800

    Removed unnecessary using directives in password_validation.cs
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- playground/csharp/password_validation.cs
+++ playground/csharp/password_validation.cs
@@ -26,13 +26,8 @@
  */
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-
-namespace Sololearn
+namespace PasswordValidation
 {
     class Program
     {
