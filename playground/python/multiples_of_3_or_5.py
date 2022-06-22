5b2ff4f3c7763e2e842f861b1584df25a4ff1d3c
Chris1320 <chris1320is@protonmail.com>
2022-06-22 22:43:44 +0800

    Added Problem #2 from Project Euler.
    + Added EvenFibonacciNumbers problem.
    + Removed semicolon on a statement from `multiples_of_3_or_5.py`
    
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- playground/python/multiples_of_3_or_5.py
+++ playground/python/multiples_of_3_or_5.py
@@ -1,5 +1,5 @@
 """
-Problem 1
+Problem 1: Multiples of 3 or 5
 
 If we list all the natural numbers below 10 that are multiples
 of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
@@ -12,7 +12,6 @@ https://projecteuler.net/problem=1
 sum = 0
 for i in range(1000):
     if (i % 3 == 0) or (i % 5 == 0):
-        sum += i;
+        sum += i
 
 print(sum)
-
