da4941d912e86050ef6540d52dcc7adc999d0456
Chris1320 <chris1320is@protonmail.com>
2022-06-26 08:31:01 +0800

    Added "Hello World" in Rust.
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

---
+++ playground/rust/hello.rs
@@ -0,0 +1,6 @@
+fn main() {  // Define the main function.
+    // Print "Hello, World!"
+    println!("Hello, World!");  // `println!` is a macro that prints a string with a newline at the
+                                // end.
+}
+
