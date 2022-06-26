80ac76fde32f4813a75bca29c9b4eb2cc2fca19f
Chris1320 <chris1320is@protonmail.com>
2022-06-26 09:21:27 +0800

    Added Rust Cargo example file structure.
    + Updated `hello.rs`
    
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- playground/rust/hello.rs
+++ playground/rust/hello.rs
@@ -1,6 +1,10 @@
-fn main() {  // Define the main function.
-    // Print "Hello, World!"
-    println!("Hello, World!");  // `println!` is a macro that prints a string with a newline at the
-                                // end.
+fn main()  // Define the main function.
+{
+    println!("Hello, World!");  // `println!` is a macro that prints a string
+                                // with a newline at the end.
+    /*
+     * Using a `!` means that you are
+     * calling a macro, and not a function.
+     */
 }
 
