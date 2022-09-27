b73f3ad748367b1ab4072de0c9089bf2c3f55a94
Chris1320 <chris1320is@protonmail.com>
2022-09-28 06:36:28 +0800

    Added Area of Rectangle exercise in C#.
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

---
+++ playground/csharp/AreaOfRectangle.cs
@@ -0,0 +1,44 @@
+/* Area of Rectangle
+ *
+ * You are writing a program to calculate
+ * the area of a rectangle. Currently it
+ * takes the the length and the height as inputs.
+ *
+ * Complete the given method to take them as
+ * arguments, then calculate and return the area.
+ *
+ * Sample Input
+ * 4
+ * 5
+ *
+ * Sample Output
+ * 20
+ */
+
+using System;
+
+namespace AreaOfRectangle
+{
+    class Program
+    {
+        static double getArea(double length, double height)
+        {
+            return length * height;
+        }
+
+        static int Main(string[] args)
+        {
+            double length, height;
+
+            // Get the rectangle's length and height.
+            Console.Write("Enter the length of the rectangle: ");
+            length = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the height of the rectangle: ");
+            height = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"The area of a {length}x{height} rectangle is {getArea(length, height)}.");
+
+            return 0;
+        }
+    }
+}
