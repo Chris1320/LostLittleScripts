bd74eb3def1b3cca51968133f32de04c4f94c330
Chris1320 <chris1320is@protonmail.com>
2022-10-31 10:58:36 +0800

    Added Python solution to SoloLearn Area of Rectangle.

---
+++ sololearn/area_of_rectangle/solution.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def main():
+    height: float = float(input("Enter the height of the rectangle: "))
+    width: float = float(input("Enter the width of the rectangle: "))
+    print(f"The area of a {height}x{width} rectangle is {height * width}.")
+
+
+if __name__ == "__main__":
+    main()
