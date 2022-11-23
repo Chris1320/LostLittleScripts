3926f05b2b9f84835b79c259da8c96096437dc19
Chris1320 <chris1320is@protonmail.com>
2022-11-23 09:37:59 +0800

    Added `CharacterValues.c`
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

---
+++ playground/c/CharacterValues.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    char c;
+
+    printf("Enter a character: ");
+    scanf("%c", &c);
+    printf("\nCharacter: %c\n", c);
+    printf("Character Value: %i\n", c);
+
+    return 0;
+}
