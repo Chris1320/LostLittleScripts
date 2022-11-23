6d4adcffc9622cae000f528a7e7e46956ff89815
Chris1320 <chris1320is@protonmail.com>
2022-11-23 19:32:45 +0800

    Updated `HelloWorld.cpp`
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- playground/cpp/HelloWorld.cpp
+++ playground/cpp/HelloWorld.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+//using namespace std;
+// The above statement lets you omit `std::` tokens below,
+// allowing you to call `cout` and `endl`.
+
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
