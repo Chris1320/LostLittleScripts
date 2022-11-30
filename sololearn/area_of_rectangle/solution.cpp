9cce9504af39aba109159e9a1fdb1966fd16e942
Chris1320 <chris1320is@protonmail.com>
2022-12-01 06:46:52 +0800

    Added C++ solution for "Area of Rectangle" SoloLearn exercise.
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

---
+++ sololearn/area_of_rectangle/solution.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+int main()
+{
+    int x, y;
+    cout << "Enter number x: ";
+    cin >> x;
+    cout << "Enter number y: ";
+    cin >> y;
+    cout << x * y << std::endl;
+}
