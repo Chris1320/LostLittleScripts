65d526ff0823b24baa92806d94f9a4adde798d7b
Chris1320 <chris1320is@protonmail.com>
2022-12-04 16:09:04 +0800

    Refactored Day 4 solutions of Advent of Code.
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- advent-of-code/2022/day4/part2/solution.py
+++ advent-of-code/2022/day4/part2/solution.py
@@ -4,32 +4,17 @@ import sys
 
 def main() -> int:
     with open(os.path.join("..", "list.txt"), 'r') as f:
-        list = f.read().split('\n')
+        contents = f.read().split('\n')
 
     duplicates = 0
-    for pairs in list:
-        pairs = pairs.split(',')
-        pair1 = pairs[0].partition('-')
-        pair2 = pairs[1].partition('-')
-        pair1_contents = [i for i in range(int(pair1[0]), int(pair1[2]) + 1)]
-        pair2_contents = [i for i in range(int(pair2[0]), int(pair2[2]) + 1)]
-        minimum_length = min((min(pair1_contents), min(pair2_contents)))
-        maximum_length = max((max(pair1_contents), max(pair2_contents)))
-
-        # The following is for visualization purposes only. It can be removed without any consequences.
-        output = ["", ""]
-        for idx in range(minimum_length, maximum_length + 1):
-            output[0] += str(idx) if idx in pair1_contents else '.'
-            output[1] += str(idx) if idx in pair2_contents else '.'
-
-        print(output[0])
-        print(output[1])
-        # print(pair1_contents)
-        # print(pair2_contents)
-        print('=' * 40)
+    for pairs in contents:
+        pair1 = list(map(int, pairs.split(',')[0].split('-')))  # use split instead of partition to remove separator in one line
+        pair2 = list(map(int, pairs.split(',')[1].split('-')))
+        pair1_contents = [i for i in range(pair1[0], pair1[1] + 1)]
+        pair2_contents = [i for i in range(pair2[0], pair2[1] + 1)]
 
         # Evaluate if the start and end of each pair is in each pairs' contents.
-        if len(set(pair1_contents) & set(pair2_contents)) != 0:
+        if len(set(pair1_contents) & set(pair2_contents)) > 0:
             duplicates += 1
 
     print()
