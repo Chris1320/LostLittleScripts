d601daa13eb056f9fc1c51ec5991eda31b749a9d
Chris1320 <chris1320is@protonmail.com>
2022-12-11 12:15:08 +0800

    Updated `string_formatting.rs`
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- playground/rust/string_formatting.rs
+++ playground/rust/string_formatting.rs
@@ -2,5 +2,13 @@ fn main()
 {
     // println!(15)  // invalid
     println!("{}", 15);  // valid
+
+    // Basic formatting
+    println!("{}, {}!", "Hello", "World");
+
+    // Positional formatting
     println!("{0}, {1}! {0}, {2}!", "Hello", "World", "Universe");
+
+    // Named formatting
+    println!("The color of the {animal} is {color}.", animal="Dog", color="Brown");
 }
