c2a3abfd905c9d4e6dbd2f2c1c5a84d2b40629b9
Chris1320 <chris1320is@protonmail.com>
2022-12-11 12:21:51 +0800

    Updated `string_formatting.rs`
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- playground/rust/string_formatting.rs
+++ playground/rust/string_formatting.rs
@@ -1,5 +1,7 @@
 fn main()
 {
+    let x = (128, 3.1415, true, "A String!");  // This will be used in one of the examples below.
+
     // println!(15)  // invalid
     println!("{}", 15);  // valid
 
@@ -10,5 +12,11 @@ fn main()
     println!("{0}, {1}! {0}, {2}!", "Hello", "World", "Universe");
 
     // Named formatting
-    println!("The color of the {animal} is {color}.", animal="Dog", color="Brown");
+    println!("The color of the {animal} is {color}.", animal="dog", color="brown");
+
+    // Placeholder traits
+    println!("The binary, octal, and hexadecimal forms of the number {0} are {0:b}, {0:o}, and {0:x} respectively.", 75);
+
+    // Placeholder traits (Debugging)
+    println!("These are the contents of the tuple x: {:?}", x);
 }
