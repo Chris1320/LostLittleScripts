d51e98297b33040afeb4efa5d55f747d8a5ad7d3
Chris1320 <chris1320is@protonmail.com>
2023-02-08 15:29:10 +0800

    Updated `hello.asm`.
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- playground/asm/hello.asm
+++ playground/asm/hello.asm
@@ -5,12 +5,17 @@
 ; $ nasm -f elf32 -o hello.o hello.asm
 ; $ ld -m elf_i386 -o hello hello.o
 
-global _start  ; Define _start label.
+section .data:  ; This section contains variable definitions.
+    ; Create a new variable `msg` with the value `Hello, World!` with a newline at the end.
+    msg: db "Hello, World!", 0xA  ; 0xA is a newline character.
+    msg_length: equ $-msg  ; Set the variable `msg_length` to have the length of `msg`.
 
 section .text:  ; This section contains program instructions.
-    _start:  ; The start label.
+    global _start  ; Define _start label.
+
+_start:  ; The start label.
         mov eax, 0x4         ; 0x4 is the write syscall.
-        mov ebx, 1           ; Use stdout as file descriptor.
+        mov ebx, 0x1           ; Use stdout as file descriptor.
         mov ecx, msg         ; Use msg as buffer.
         mov edx, msg_length  ; Use msg_length as buffer length.
 
@@ -19,8 +24,3 @@ section .text:  ; This section contains program instructions.
         mov eax, 0x1  ; 0x1 is the exit syscall.
         mov ebx, 0x0  ; 0x0 will return an exit code of 0.
         int 0x80  ; Run the exit syscall.
-
-section .data:  ; This section contains variable definitions.
-    ; Create a new variable `msg` with the value `Hello, World!` with a newline at the end.
-    msg: db "Hello, World!", 0xA  ; 0xA is a newline character.
-    msg_length equ $-msg  ; Set the variable `msg_length` to have the length of `msg`.
