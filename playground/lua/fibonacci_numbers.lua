56dc462e22db741d9d7c7800bd623b9a290a125f
Chris1320 <chris1320is@protonmail.com>
2023-05-30 10:34:21 +0800

    Added `fibonacci_numbers.lua`.
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

---
+++ playground/lua/fibonacci_numbers.lua
@@ -0,0 +1,43 @@
+-- Get the next fibonacci number from x and y.
+local function next(x, y)
+    return x + y
+end
+
+local function main()
+    io.write("How many fibonacci numbers do you want to see? > ")
+    local nums = io.read('n')
+
+    -- Set up the first three fibonacci numbers.
+    local x = 1
+    local y = 1
+    local z = next(x, y)
+    local n = 0
+
+    io.write("\nListing " .. nums .. " Fibonacci numbers...\n\n")
+
+    if nums >= 1 then
+        n = n + 1
+        io.write('[' .. n .. '] ' .. x .. '\n')
+    end
+
+    if nums >= 2 then
+        n = n + 1
+        io.write('[' .. n .. '] ' .. y .. '\n')
+    end
+
+    if nums >= 3 then
+        n = n + 1
+        io.write('[' .. n .. '] ' .. z .. '\n')
+    end
+
+    while n < nums do
+        x = y
+        y = z
+        z = next(x, y)
+        n = n + 1
+        io.write('[' .. n .. '] ' .. z .. '\n')
+    end
+    return 0
+end
+
+os.exit(main())
