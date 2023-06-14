21c1c2e62b233d4b77a85b6d6deaaafb080271a5
Chris1320 <chris1320is@protonmail.com>
2023-06-15 02:24:32 +0800

    Updated `TypeConversion.cs`
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- playground/csharp/TypeConversion.cs
+++ playground/csharp/TypeConversion.cs
@@ -9,12 +9,26 @@ namespace TypeConversion
             int num1, num2;
             double quotient;
 
-            Console.Write("Enter int for num1: ");
-            num1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter int for num1: ");
+            Console.Write("Enter byte for num1: ");
+            // This will perform an implicit type conversion
+            // because the input is converted into `byte`,
+            // but the variable is of `int` datatype.
+            //
+            // Example value:                          1
+            // byte:                            00000001
+            // int:  00000000 00000000 00000000 00000001
+            num1 = Convert.ToByte(Console.ReadLine());
+            Console.Write("Enter int for num2: ");
+            // The `Convert` class is used for converting
+            // non-compatible types, which in this case
+            // is a string being converted into an integer.
+            //
+            // Another option is the datatype's `.Parse()`
+            // method.
             num2 = Convert.ToInt32(Console.ReadLine());
 
-            quotient = (double) num1 / num2;  // Explicit type conversion
+            // Explicit type conversion, a.k.a Casting
+            quotient = (double) num1 / num2;
             Console.WriteLine(quotient);
 
             return 0;
