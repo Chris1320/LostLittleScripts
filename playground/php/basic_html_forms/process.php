a46fe6811e214ca5c2744effc84a76ac3d67515b
Chris1320 <chris1320is@protonmail.com>
2023-07-14 11:40:51 +0800

    Added `Basic HTML/PHP Form` activity.
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

---
+++ playground/php/basic_html_forms/process.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Activity 14.2: Basic HTML/PHP Forms</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <p>First Name: <?php echo $_POST["first_name"]; ?></p>
+        <p>Last Name: <?php echo $_POST["last_name"]; ?></p>
+    </body>
+</html>
