2d70d335facc7cf74a497e387e7ae1c81c165c63
chris1320 <chris1320is@protonmail.com>
2023-09-20 18:44:09 +0800

    Added activity description in Employee Salary Calculator VB.NET program.
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

--- playground/vb.net/WinForms/EmployeeSalaryCalculator/EmployeeSalaryCalculator/Form1.vb
+++ playground/vb.net/WinForms/EmployeeSalaryCalculator/EmployeeSalaryCalculator/Form1.vb
@@ -1,4 +1,12 @@
-﻿Public Class Form1
+﻿' Employee Salary Calculator
+'
+' An employee working at a small company earns 100 pesos on his
+' first day. However, for every day that passes, their base amount
+' doubles, so they earn 200 on the second day and 400 on the third
+' day (for a total of 700 pesos). Given a number of days, compute
+' how much will he earn.
+
+Public Class Form1
     ' Compute the total salary of an employee.
     ' num_days: number of days worked
     ' base_salary: base salary of the employee
@@ -9,11 +17,11 @@
             total_salary += base_salary
             base_salary *= 2
         Next
+
         Return total_salary
     End Function
 
     Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles numDays.ValueChanged
-        ' if numDays is equal to 1 then lblDays.Text = "day" else lblDays.Text = "days"
         Dim day_grammar As String = If(numDays.Value = 1, "day", "days")
         lblDesc.Text = String.Format("For the {0} {1} you've worked, you earned a total of", numDays.Value, day_grammar)
         Dim total As Double = GetTotalSalary(numDays.Value, 100)
