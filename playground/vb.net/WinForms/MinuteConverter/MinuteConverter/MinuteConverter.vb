176b815914fdb38f04427cfe86fb950a530a006c
chris1320 <chris1320is@protonmail.com>
2023-09-20 21:56:51 +0800

    Updated VB.NET MinuteConverter program.
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

--- playground/vb.net/WinForms/MinuteConverter/MinuteConverter/MinuteConverter.vb
+++ playground/vb.net/WinForms/MinuteConverter/MinuteConverter/MinuteConverter.vb
@@ -4,7 +4,7 @@
 '
 'Example: 3 hours And 20 minutes Is equal to 200 minutes.
 
-Public Class Form1
+Public Class MinuteConverter
     Private Sub txtHours_TextChanged(sender As Object, e As EventArgs) Handles txtHours.TextChanged
         txtTotalMinutes.Text = Val(txtHours.Text) * 60 + Val(txtMinutes.Text)
     End Sub
