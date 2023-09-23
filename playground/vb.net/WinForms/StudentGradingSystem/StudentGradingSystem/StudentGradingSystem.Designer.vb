0c6ad332cafa95db1826fd3baf847a640ab48479
chris1320 <chris1320is@protonmail.com>
2023-09-23 23:41:39 +0800

    Added VB.NET Student Grading System Activity.
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

---
+++ playground/vb.net/WinForms/StudentGradingSystem/StudentGradingSystem/StudentGradingSystem.Designer.vb
@@ -0,0 +1,311 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class StudentGradingSystem
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TableLayoutPanel1 = New TableLayoutPanel()
+        Label12 = New Label()
+        Label11 = New Label()
+        Label10 = New Label()
+        Label9 = New Label()
+        Label8 = New Label()
+        Label7 = New Label()
+        Label6 = New Label()
+        Label5 = New Label()
+        Label4 = New Label()
+        Label2 = New Label()
+        Label1 = New Label()
+        Label3 = New Label()
+        numStudentGrade = New NumericUpDown()
+        Label13 = New Label()
+        Label14 = New Label()
+        lblResult = New Label()
+        Button1 = New Button()
+        Label15 = New Label()
+        TableLayoutPanel1.SuspendLayout()
+        CType(numStudentGrade, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' TableLayoutPanel1
+        ' 
+        TableLayoutPanel1.ColumnCount = 2
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 50F))
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 50F))
+        TableLayoutPanel1.Controls.Add(Label12, 1, 5)
+        TableLayoutPanel1.Controls.Add(Label11, 0, 5)
+        TableLayoutPanel1.Controls.Add(Label10, 1, 4)
+        TableLayoutPanel1.Controls.Add(Label9, 0, 4)
+        TableLayoutPanel1.Controls.Add(Label8, 1, 3)
+        TableLayoutPanel1.Controls.Add(Label7, 0, 3)
+        TableLayoutPanel1.Controls.Add(Label6, 1, 2)
+        TableLayoutPanel1.Controls.Add(Label5, 0, 2)
+        TableLayoutPanel1.Controls.Add(Label4, 1, 1)
+        TableLayoutPanel1.Controls.Add(Label2, 1, 0)
+        TableLayoutPanel1.Controls.Add(Label1, 0, 0)
+        TableLayoutPanel1.Controls.Add(Label3, 0, 1)
+        TableLayoutPanel1.Location = New Point(34, 93)
+        TableLayoutPanel1.Name = "TableLayoutPanel1"
+        TableLayoutPanel1.RowCount = 6
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 25F))
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 15F))
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 15F))
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 15F))
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 15F))
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 15F))
+        TableLayoutPanel1.Size = New Size(195, 147)
+        TableLayoutPanel1.TabIndex = 0
+        ' 
+        ' Label12
+        ' 
+        Label12.Anchor = AnchorStyles.None
+        Label12.AutoSize = True
+        Label12.Location = New Point(132, 128)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(28, 15)
+        Label12.TabIndex = 11
+        Label12.Text = "5.00"
+        Label12.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label11
+        ' 
+        Label11.Anchor = AnchorStyles.None
+        Label11.AutoSize = True
+        Label11.Location = New Point(21, 128)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(54, 15)
+        Label11.TabIndex = 10
+        Label11.Text = "Below 60"
+        Label11.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label10
+        ' 
+        Label10.Anchor = AnchorStyles.None
+        Label10.AutoSize = True
+        Label10.Location = New Point(132, 105)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(28, 15)
+        Label10.TabIndex = 9
+        Label10.Text = "4.00"
+        Label10.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label9
+        ' 
+        Label9.Anchor = AnchorStyles.None
+        Label9.AutoSize = True
+        Label9.Location = New Point(27, 105)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(42, 15)
+        Label9.TabIndex = 8
+        Label9.Text = "60 - 69"
+        Label9.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label8
+        ' 
+        Label8.Anchor = AnchorStyles.None
+        Label8.AutoSize = True
+        Label8.Location = New Point(132, 83)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(28, 15)
+        Label8.TabIndex = 7
+        Label8.Text = "3.00"
+        Label8.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label7
+        ' 
+        Label7.Anchor = AnchorStyles.None
+        Label7.AutoSize = True
+        Label7.Location = New Point(27, 83)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(42, 15)
+        Label7.TabIndex = 6
+        Label7.Text = "70 - 79"
+        Label7.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label6
+        ' 
+        Label6.Anchor = AnchorStyles.None
+        Label6.AutoSize = True
+        Label6.Location = New Point(132, 61)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(28, 15)
+        Label6.TabIndex = 5
+        Label6.Text = "2.00"
+        Label6.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label5
+        ' 
+        Label5.Anchor = AnchorStyles.None
+        Label5.AutoSize = True
+        Label5.Location = New Point(27, 61)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(42, 15)
+        Label5.TabIndex = 4
+        Label5.Text = "80 - 89"
+        Label5.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label4
+        ' 
+        Label4.Anchor = AnchorStyles.None
+        Label4.AutoSize = True
+        Label4.Location = New Point(132, 39)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(28, 15)
+        Label4.TabIndex = 3
+        Label4.Text = "1.00"
+        Label4.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label2
+        ' 
+        Label2.Anchor = AnchorStyles.None
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.Location = New Point(118, 7)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(55, 21)
+        Label2.TabIndex = 1
+        Label2.Text = "Grade"
+        Label2.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label1
+        ' 
+        Label1.Anchor = AnchorStyles.None
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(19, 7)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(58, 21)
+        Label1.TabIndex = 0
+        Label1.Text = "Range"
+        Label1.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label3
+        ' 
+        Label3.Anchor = AnchorStyles.None
+        Label3.AutoSize = True
+        Label3.Location = New Point(9, 39)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(79, 15)
+        Label3.TabIndex = 2
+        Label3.Text = "90 and Above"
+        Label3.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' numStudentGrade
+        ' 
+        numStudentGrade.DecimalPlaces = 2
+        numStudentGrade.Location = New Point(306, 132)
+        numStudentGrade.Name = "numStudentGrade"
+        numStudentGrade.Size = New Size(120, 23)
+        numStudentGrade.TabIndex = 1
+        ' 
+        ' Label13
+        ' 
+        Label13.AutoSize = True
+        Label13.Font = New Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point)
+        Label13.Location = New Point(279, 101)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(188, 20)
+        Label13.TabIndex = 2
+        Label13.Text = "Student's Numerical Grade:"
+        ' 
+        ' Label14
+        ' 
+        Label14.AutoSize = True
+        Label14.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        Label14.Location = New Point(359, 190)
+        Label14.Name = "Label14"
+        Label14.Size = New Size(67, 25)
+        Label14.TabIndex = 3
+        Label14.Text = "Grade:"
+        ' 
+        ' lblResult
+        ' 
+        lblResult.AutoSize = True
+        lblResult.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        lblResult.Location = New Point(432, 190)
+        lblResult.Name = "lblResult"
+        lblResult.Size = New Size(0, 25)
+        lblResult.TabIndex = 4
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
+        Button1.Location = New Point(259, 183)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 42)
+        Button1.TabIndex = 5
+        Button1.Text = "Compute"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label15
+        ' 
+        Label15.AutoSize = True
+        Label15.Font = New Font("Segoe UI", 27.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label15.Location = New Point(34, 9)
+        Label15.Name = "Label15"
+        Label15.Size = New Size(441, 50)
+        Label15.TabIndex = 6
+        Label15.Text = "Student Grading System"
+        ' 
+        ' StudentGradingSystem
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(525, 346)
+        Controls.Add(Label15)
+        Controls.Add(Button1)
+        Controls.Add(lblResult)
+        Controls.Add(Label14)
+        Controls.Add(Label13)
+        Controls.Add(numStudentGrade)
+        Controls.Add(TableLayoutPanel1)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        MaximizeBox = False
+        Name = "StudentGradingSystem"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Student Grading System"
+        TableLayoutPanel1.ResumeLayout(False)
+        TableLayoutPanel1.PerformLayout()
+        CType(numStudentGrade, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents numStudentGrade As NumericUpDown
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents lblResult As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label15 As Label
+End Class
---
+++ playground/vb.net/WinForms/StudentGradingSystem/StudentGradingSystem/StudentGradingSystem.vb
@@ -0,0 +1,18 @@
+﻿Public Class StudentGradingSystem
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Select Case numStudentGrade.Value
+            Case 0 To 59
+                lblResult.Text = "5.00"
+            Case 60 To 69
+                lblResult.Text = "4.00"
+            Case 70 To 79
+                lblResult.Text = "3.00"
+            Case 80 To 89
+                lblResult.Text = "2.00"
+            Case 90 To 100
+                lblResult.Text = "1.00"
+                ' We don't need to add a default case because
+                ' because the possible value is thin 0 to 100.
+        End Select
+    End Sub
+End Class
