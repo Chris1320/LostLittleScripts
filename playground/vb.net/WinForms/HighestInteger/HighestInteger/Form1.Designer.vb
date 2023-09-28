89606cd4848aa021a4fccaabf77244d34b20ee1b
chris1320 <chris1320is@protonmail.com>
2023-09-28 23:43:06 +0800

    Added Highest Integer VB.NET activity.
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

---
+++ playground/vb.net/WinForms/HighestInteger/HighestInteger/Form1.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        listNums = New ListBox()
+        txtNum = New TextBox()
+        btnAdd = New Button()
+        lblHighest = New Label()
+        btnClear = New Button()
+        SuspendLayout()
+        ' 
+        ' listNums
+        ' 
+        listNums.FormattingEnabled = True
+        listNums.ItemHeight = 15
+        listNums.Location = New Point(12, 75)
+        listNums.Name = "listNums"
+        listNums.Size = New Size(270, 154)
+        listNums.TabIndex = 3
+        ' 
+        ' txtNum
+        ' 
+        txtNum.Location = New Point(12, 12)
+        txtNum.Name = "txtNum"
+        txtNum.Size = New Size(189, 23)
+        txtNum.TabIndex = 0
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.Location = New Point(207, 12)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(24, 23)
+        btnAdd.TabIndex = 1
+        btnAdd.Text = "+"
+        btnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' lblHighest
+        ' 
+        lblHighest.Anchor = AnchorStyles.None
+        lblHighest.AutoSize = True
+        lblHighest.Location = New Point(55, 48)
+        lblHighest.Name = "lblHighest"
+        lblHighest.Size = New Size(176, 15)
+        lblHighest.TabIndex = 3
+        lblHighest.Text = "There are no numbers in the list."
+        lblHighest.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Location = New Point(237, 12)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(45, 23)
+        btnClear.TabIndex = 2
+        btnClear.Text = "Clear"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(294, 236)
+        Controls.Add(btnClear)
+        Controls.Add(lblHighest)
+        Controls.Add(btnAdd)
+        Controls.Add(txtNum)
+        Controls.Add(listNums)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        MaximizeBox = False
+        Name = "Form1"
+        ShowIcon = False
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Highest Number"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents listNums As ListBox
+    Friend WithEvents txtNum As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents lblHighest As Label
+    Friend WithEvents btnClear As Button
+End Class
---
+++ playground/vb.net/WinForms/HighestInteger/HighestInteger/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+    Dim highest As Double = Nothing
+    Dim free As Integer = 10
+
+    Private Sub add()
+        Dim value As Double
+        ' Only allow up to 10 items in the listbox.
+        If free < 1 Then : MessageBox.Show("The list is full.")
+        End If
+
+        ' Check if the input is a number.
+        If IsNumeric(txtNum.Text) Then : value = CDbl(txtNum.Text)
+        Else
+            MessageBox.Show("Please enter a number.")
+            Return
+        End If
+
+        If highest = Nothing Or highest < value Then
+            highest = value
+            lblHighest.Text = String.Format("The highest number is {0}.", highest)
+        End If
+
+        ' Add the number to the listbox.
+        listNums.Items.Add(value)
+        txtNum.Clear()
+        free -= 1
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        add()
+    End Sub
+
+    Private Sub txtNum_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtNum.KeyPress
+        If e.KeyChar = ChrW(Keys.Enter) Then
+            add()
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        free = 10
+        highest = Nothing
+        txtNum.Clear()
+        listNums.Items.Clear()
+        lblHighest.Text = "There are no numbers in the list."
+    End Sub
+End Class
