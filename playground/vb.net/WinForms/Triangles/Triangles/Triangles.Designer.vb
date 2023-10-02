d25b93d89843dd221ec78624edab7498dca7f3e2
chris1320 <chris1320is@protonmail.com>
2023-10-02 18:38:06 +0800

    Added Triangles VB.NET activity.
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

---
+++ playground/vb.net/WinForms/Triangles/Triangles/Triangles.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Triangles
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtResult = New TextBox()
+        Label1 = New Label()
+        txtLayers = New TextBox()
+        btnPrint = New Button()
+        SuspendLayout()
+        ' 
+        ' txtResult
+        ' 
+        txtResult.Location = New Point(12, 54)
+        txtResult.Multiline = True
+        txtResult.Name = "txtResult"
+        txtResult.ReadOnly = True
+        txtResult.Size = New Size(776, 384)
+        txtResult.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(155, 24)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(129, 15)
+        Label1.TabIndex = 1
+        Label1.Text = "Enter number of layers:"
+        ' 
+        ' txtLayers
+        ' 
+        txtLayers.Location = New Point(290, 21)
+        txtLayers.Name = "txtLayers"
+        txtLayers.Size = New Size(100, 23)
+        txtLayers.TabIndex = 2
+        ' 
+        ' btnPrint
+        ' 
+        btnPrint.Location = New Point(432, 21)
+        btnPrint.Name = "btnPrint"
+        btnPrint.Size = New Size(75, 23)
+        btnPrint.TabIndex = 3
+        btnPrint.Text = "Print"
+        btnPrint.UseVisualStyleBackColor = True
+        ' 
+        ' Triangles
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnPrint)
+        Controls.Add(txtLayers)
+        Controls.Add(Label1)
+        Controls.Add(txtResult)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        MaximizeBox = False
+        Name = "Triangles"
+        ShowIcon = False
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Triangles"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtResult As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtLayers As TextBox
+    Friend WithEvents btnPrint As Button
+End Class
---
+++ playground/vb.net/WinForms/Triangles/Triangles/Triangles.vb
@@ -0,0 +1,14 @@
+﻿Public Class Triangles
+    Private Sub btnPrint_Click(sender As Object, e As EventArgs) Handles btnPrint.Click
+        Dim layers As Integer
+        Integer.TryParse(txtLayers.Text, layers)
+        If layers > 0 Then
+            Dim triangle As String = ""
+            ' vbCrLf is the same as Environment.NewLine
+            For i As Integer = 1 To layers : triangle &= New String("*", i) & vbCrLf
+            Next
+            txtResult.Text = triangle
+        Else : txtResult.Text = "Invalid input"
+        End If
+    End Sub
+End Class
