e55c4607c49d06dbfa70d316335e8b75a76557a0
chris1320 <chris1320is@protonmail.com>
2023-10-02 18:24:19 +0800

    Added Multiplication Tables VB.NET activity.
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

---
+++ playground/vb.net/WinForms/MultiplicationTables/MultiplicationTables/MultiplicationTables.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MultiplicationTables
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        listResult = New ListBox()
+        txtInput = New TextBox()
+        btnCompute = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(12, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(244, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Enter the times table you want to learn (1-12)"
+        ' 
+        ' listResult
+        ' 
+        listResult.FormattingEnabled = True
+        listResult.ItemHeight = 15
+        listResult.Location = New Point(12, 56)
+        listResult.Name = "listResult"
+        listResult.Size = New Size(243, 184)
+        listResult.TabIndex = 1
+        ' 
+        ' txtInput
+        ' 
+        txtInput.Location = New Point(12, 27)
+        txtInput.Name = "txtInput"
+        txtInput.Size = New Size(244, 23)
+        txtInput.TabIndex = 2
+        ' 
+        ' btnCompute
+        ' 
+        btnCompute.Location = New Point(30, 246)
+        btnCompute.Name = "btnCompute"
+        btnCompute.Size = New Size(203, 23)
+        btnCompute.TabIndex = 3
+        btnCompute.Text = "Tell me my times table"
+        btnCompute.UseVisualStyleBackColor = True
+        ' 
+        ' MultiplicationTables
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(267, 280)
+        Controls.Add(btnCompute)
+        Controls.Add(txtInput)
+        Controls.Add(listResult)
+        Controls.Add(Label1)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        MaximizeBox = False
+        Name = "MultiplicationTables"
+        ShowIcon = False
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Multiplication Tables"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents listResult As ListBox
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents btnCompute As Button
+End Class
---
+++ playground/vb.net/WinForms/MultiplicationTables/MultiplicationTables/MultiplicationTables.vb
@@ -0,0 +1,17 @@
+﻿Public Class MultiplicationTables
+    Private Sub btnCompute_Click(sender As Object, e As EventArgs) Handles btnCompute.Click
+        Dim number As Integer
+
+        If Integer.TryParse(txtInput.Text, number) Then
+            If number < 1 Or number > 12 Then
+                MessageBox.Show("Please enter a number between 1 and 12.")
+                Return
+            End If
+
+            listResult.Items.Clear()
+            For i As Integer = 1 To 12 : listResult.Items.Add($"{number} x {i} = {number * i}")
+            Next
+        Else : MessageBox.Show("Please enter a valid number.")
+        End If
+    End Sub
+End Class
