2c44550c6525a4c5ee893158d0fae8a759da8a0f
chris1320 <chris1320is@protonmail.com>
2023-10-02 19:36:39 +0800

    Added Fibonacci Sequence VB.NET activity.
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

---
+++ playground/vb.net/WinForms/FibonacciSequence/FibonacciSequence/FibonacciSequence.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FibonacciSequence
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        txtLimit = New TextBox()
+        btnPrint = New Button()
+        listResult = New ListBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(25, 30)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(67, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Enter Limit:"
+        ' 
+        ' txtLimit
+        ' 
+        txtLimit.Location = New Point(98, 27)
+        txtLimit.Name = "txtLimit"
+        txtLimit.Size = New Size(100, 23)
+        txtLimit.TabIndex = 1
+        ' 
+        ' btnPrint
+        ' 
+        btnPrint.Location = New Point(204, 27)
+        btnPrint.Name = "btnPrint"
+        btnPrint.Size = New Size(75, 23)
+        btnPrint.TabIndex = 2
+        btnPrint.Text = "Print"
+        btnPrint.UseVisualStyleBackColor = True
+        ' 
+        ' listResult
+        ' 
+        listResult.FormattingEnabled = True
+        listResult.ItemHeight = 15
+        listResult.Location = New Point(25, 66)
+        listResult.Name = "listResult"
+        listResult.Size = New Size(254, 274)
+        listResult.TabIndex = 3
+        ' 
+        ' FibonacciSequence
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(305, 356)
+        Controls.Add(listResult)
+        Controls.Add(btnPrint)
+        Controls.Add(txtLimit)
+        Controls.Add(Label1)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        MaximizeBox = False
+        Name = "FibonacciSequence"
+        ShowIcon = False
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Fibonacci Sequence"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtLimit As TextBox
+    Friend WithEvents btnPrint As Button
+    Friend WithEvents listResult As ListBox
+End Class
---
+++ playground/vb.net/WinForms/FibonacciSequence/FibonacciSequence/FibonacciSequence.vb
@@ -0,0 +1,16 @@
+﻿Public Class FibonacciSequence
+    Private Sub btnPrint_Click(sender As Object, e As EventArgs) Handles btnPrint.Click
+        Dim limit As Integer
+        Integer.TryParse(txtLimit.Text, limit)
+        If limit <= 0 Then : MessageBox.Show("Please enter a positive integer.")
+        End If
+        ' Print the Fibonacci sequence that are less than or equal to the limit.
+        listResult.Items.Clear()
+        listResult.Items.Add(0)
+        listResult.Items.Add(1)
+        listResult.Items.Add(1)
+        While listResult.Items(listResult.Items.Count - 1) + listResult.Items(listResult.Items.Count - 2) <= limit
+            listResult.Items.Add(listResult.Items(listResult.Items.Count - 1) + listResult.Items(listResult.Items.Count - 2))
+        End While
+    End Sub
+End Class
