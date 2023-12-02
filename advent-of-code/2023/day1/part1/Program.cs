a3b3b58728de028fa7be82afb33b39bc339b3f40
chris1320 <chris1320is@protonmail.com>
2023-12-02 17:14:55 +0800

    Added Advent of Code 2023: Day 1 (part 1)
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

---
+++ advent-of-code/2023/day1/part1/Program.cs
@@ -0,0 +1,44 @@
+﻿class Program
+{
+    public static int Main(string[] args)
+    {
+        int sum = 0;
+        int lines = 0;
+        using (var reader = new StreamReader("../list.txt"))
+        {
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                string result = String.Empty;
+
+                if (line != null)
+                {
+                    char[] row_chars = line.ToCharArray();
+                    foreach (var c in row_chars)
+                        if (Char.IsNumber(c))
+                        {
+                            result += Convert.ToString(c);
+                            break;
+                        }
+
+                    Array.Reverse(row_chars);
+                    foreach (var c in row_chars)
+                        if (Char.IsNumber(c))
+                        {
+                            result += Convert.ToString(c);
+                            break;
+                        }
+
+                    if (!String.IsNullOrEmpty(result))
+                        sum += Convert.ToInt32(result);
+
+                    lines++;
+                }
+            }
+        }
+
+        Console.WriteLine($"Processed {lines} lines.");
+        Console.WriteLine($"Total: {sum}");
+        return 0;
+    }
+}
