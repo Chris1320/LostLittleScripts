5f3398837deb6534db7b0600e3203586a8e77767
Chris1320 <chris1320is@protonmail.com>
2023-12-04 15:48:48 +0800

    refactor AoC day 1 part 1 solution
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- advent-of-code/2023/day1/part1/Program.cs
+++ advent-of-code/2023/day1/part1/Program.cs
@@ -2,43 +2,54 @@
 {
     public static int Main(string[] args)
     {
-        int sum = 0;
-        int lines = 0;
-        using (var reader = new StreamReader("../list.txt"))
+        if (args.Length == 0)
         {
-            while (!reader.EndOfStream)
-            {
-                var line = reader.ReadLine();
-                string result = String.Empty;
+            Console.WriteLine("Usage: part1 <input_file>");
+            return 1;
+        }
 
-                if (line != null)
+        uint sum = 0;
+        uint lines = 0;
+
+        try
+        {
+            using (var reader = new StreamReader(args[0]))
+            {
+                while (!reader.EndOfStream)
                 {
+                    var line = reader.ReadLine();
+                    uint result = 0;
+
+                    Console.WriteLine($"Processing line #{++lines}: {line}");
+                    if (String.IsNullOrWhiteSpace(line))
+                        continue;
+
                     char[] row_chars = line.ToCharArray();
                     foreach (var c in row_chars)
                         if (Char.IsNumber(c))
                         {
-                            result += Convert.ToString(c);
+                            result += (uint)Char.GetNumericValue(c) * 10;
                             break;
                         }
-
-                    Array.Reverse(row_chars);
-                    foreach (var c in row_chars)
-                        if (Char.IsNumber(c))
+                    for (int i = row_chars.Length - 1; i >= 0; i--)
+                        if (Char.IsNumber(row_chars[i]))
                         {
-                            result += Convert.ToString(c);
+                            result += (uint)Char.GetNumericValue(row_chars[i]);
                             break;
                         }
 
-                    if (!String.IsNullOrEmpty(result))
-                        sum += Convert.ToInt32(result);
-
-                    lines++;
+                    sum += result;
                 }
             }
-        }
 
-        Console.WriteLine($"Processed {lines} lines.");
-        Console.WriteLine($"Total: {sum}");
-        return 0;
+            Console.WriteLine($"Processed {lines} lines.");
+            Console.WriteLine($"Total: {sum}");
+            return 0;
+        }
+        catch (FileNotFoundException e)
+        {
+            Console.WriteLine(e.Message);
+            return 1;
+        }
     }
 }
